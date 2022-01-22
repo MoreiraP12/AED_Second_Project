9519b5654528ef8783b3ef6a14b01e711da94cf0
Pedro Barbeira <up201303693@fe.up.pt>
2022-01-22 22:49:27 +0000

    And we're on

---
+++ cmake-build-debug/CMakeFiles/aed_proj_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aed_proj_2.dir/main.cpp.obj"
+  "aed_proj_2.exe"
+  "aed_proj_2.exe.manifest"
+  "aed_proj_2.pdb"
+  "libaed_proj_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aed_proj_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
